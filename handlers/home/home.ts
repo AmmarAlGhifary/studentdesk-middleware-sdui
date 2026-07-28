612f2e8408086f35e3535f6b3aee90f44897884e
Ammar <ammaralghifary@gmail.com>
2026-07-29 02:58:09 +0700

    Skenario 3 Iterasi 8

--- handlers/home/home.ts
+++ handlers/home/home.ts
@@ -69,7 +69,7 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
                     // Jadwal Kuliah Section
                     {
                         type: "section_header",
-                        title: "Jadwal Kuliah Hari Ini",
+                        title: "Jadwal Kelas Hari Ini",
                         modifier: SduiTheme.modifiers.sectionHeader
                     },
                     ...(scheduleCards.length > 0 ? [{
